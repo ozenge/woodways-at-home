68b058a6fa5898c164456be3874e632bec7c132c
ozenge <151889224+ozenge@users.noreply.github.com>
2025-01-07 00:04:17 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -106,7 +106,7 @@ let draw=(g,s)=>{
 		ctx.strokeStyle="rgba(0,0,0,0.2)";ctx.lineWidth=10;                                          //bg
 		ctx.fillStyle=(!x.t)?co.W:/[wi]/.test(x.t)?co[x.t]:co["l"+(x.s||s)];ctx.fillRect(dx,dy,w+1,w+1);
 		if(x.t)ctx.strokeRect(dx,dy,w,w);ctx.fillStyle="black";dx+=w/2;dy+=w*0.75; //why?
-		ctx.font=`bold ${w*0.67}px sans-serif`;ctx.strokeStyle="white";ctx.lineWidth=5;               //fg
+		ctx.font=`${w*0.67}px sans-serif`;ctx.strokeStyle="white";ctx.lineWidth=3;               //fg
 		if(/[rgp]/.test(x.t)){let p=[em(x.l==gg&&x.u?"s":x.t),dx,dy];ctx.strokeText(...p);ctx.fillText(...p);}
 		if(x.l&&x.t!="r")    {let p=[x.l==gg?"gg":x.l,dx,dy];        ctx.strokeText(...p);ctx.fillText(...p);}
 		if(x.a)ctx.fillText(em(x.a),dx,dy);
